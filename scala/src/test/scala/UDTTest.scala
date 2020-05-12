e46ac0970c28a3dda3c67654bf7c9c247cc796e5
bigsheeper <42060877+bigsheeper@users.noreply.github.com>
2020-05-12 17:27:22 +0800

    fix scala test

--- scala/src/test/scala/UDTTest.scala
+++ scala/src/test/scala/UDTTest.scala
@@ -8,7 +8,6 @@ import org.locationtech.jts.io.WKTReader
 
 class UDTTest extends FunSuite {
   test("GeometryUDT") {
-    import org.apache.spark.sql.udt.geometry._
 
     Logger.getLogger("org").setLevel(Level.WARN)
     val spark = SparkSession.builder()
@@ -33,34 +32,4 @@ class UDTTest extends FunSuite {
 
     spark.stop()
   }
-
-  test("PointUDT") {
-    import org.apache.spark.sql.udt.point._
-
-    Logger.getLogger("org").setLevel(Level.WARN)
-    val spark = SparkSession.builder()
-      .master("local[*]")
-      .appName("point_udt_test")
-      .getOrCreate()
-
-    val data = Seq(
-      Row(1, new ArcternPoint(new GeometryFactory().createPoint(new Coordinate(1, 1)))),
-      Row(2, new ArcternPoint(new GeometryFactory().createPoint(new Coordinate(2, 2)))),
-      Row(3, new ArcternPoint(new GeometryFactory().createPoint(new Coordinate(3, 3)))),
-      Row(4, new ArcternPoint(new GeometryFactory().createPoint(new Coordinate(4, 4)))),
-      Row(5, new ArcternPoint(new GeometryFactory().createPoint(new Coordinate(5, 5))))
-    )
-
-    val rdd_d = spark.sparkContext.parallelize(data)
-    val schema = StructType(Array(StructField("idx", IntegerType, nullable = false), StructField("point", new PointUDT, nullable = false)))
-    val df = spark.createDataFrame(rdd_d, schema)
-    df.createOrReplaceTempView("data")
-
-    val rst = spark.sql("select * from data")
-    rst.queryExecution.debug.codegen()
-    rst.show()
-
-    spark.stop()
-  }
-
 }
