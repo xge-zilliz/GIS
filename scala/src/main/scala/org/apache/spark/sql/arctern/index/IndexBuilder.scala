955eef05d7561ed0bc61b40c1c8b2340ba74f32c
xge-zilliz <xi.ge@zilliz.com>
2020-05-22 16:24:58 +0800

    add indexjoin

--- scala/src/main/scala/org/apache/spark/sql/arctern/index/IndexBuilder.scala
+++ scala/src/main/scala/org/apache/spark/sql/arctern/index/IndexBuilder.scala
@@ -4,11 +4,17 @@ import java.util
 
 import org.apache.spark.sql.arctern.GeometryUDT
 import org.locationtech.jts.geom.{Envelope, Geometry}
+import org.locationtech.jts.index.quadtree.Quadtree
 import org.locationtech.jts.index.strtree.STRtree
 import org.locationtech.jts.index.{ItemVisitor, SpatialIndex}
 
-class IndexBuilder extends SpatialIndex with Serializable {
-  val index = new STRtree()
+class IndexBuilder(indexType: String) extends SpatialIndex with Serializable {
+
+  var index:SpatialIndex = null
+  if (indexType.equalsIgnoreCase("RTREE"))
+    index = new STRtree()
+  else
+    index = new Quadtree()
 
   override def insert(itemEnv: Envelope, item: Any): Unit = index.insert(itemEnv, item)
 
--- scala/src/test/scala/IndexTest.scala
+++ scala/src/test/scala/IndexTest.scala
@@ -15,14 +15,14 @@
  */
 
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.arctern.expressions.JoinCase
+import org.apache.spark.sql.arctern.expressions.IndexedJoin
 import org.apache.spark.sql.arctern.index.{IndexBuilder, RTreeIndex}
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.locationtech.jts.io.WKTReader
 
 class IndexTest extends AdapterTest {
 
-  test("testsql") {
+  test("test index") {
     val index = new RTreeIndex
     val point1 = new WKTReader().read("POINT(-100 -40)")
     val point2 = new WKTReader().read("POINT(100 -40)")
@@ -58,7 +58,7 @@ class IndexTest extends AdapterTest {
   }
 
   test("Indextest") {
-    val index = new IndexBuilder
+    val index = new IndexBuilder("RTREE")
     val data = Seq(
       Row(1, "POINT(-100 -40)", "POLYGON((-180 -90, -20 -60, 0 0, -180 0, -180 -90))"),
       Row(2, "POINT(100 -40)", "POLYGON((0 -90, 180 -90, 180 0, 0 0, 0 -90))"),
@@ -75,7 +75,7 @@ class IndexTest extends AdapterTest {
 
     val broadcastVar = spark.sparkContext.broadcast(index)
     println(broadcastVar.value)
-    val joincase = new JoinCase(broadcastVar)
+    val joincase = new IndexedJoin(broadcastVar)
     val result = joincase.join(geo1)
     result.foreach{ out =>
       println(out)
@@ -83,6 +83,46 @@ class IndexTest extends AdapterTest {
   }
 
   test("test1 big data") {
+    val resourceFolder = System.getProperty("user.dir") + "/src/test/resources/"
+    val PointLocation = resourceFolder + "point.csv"
+    val PolygonLocation = resourceFolder + "polygon.csv"
 
+    val conf_array = spark.sparkContext.getConf.getAll
+    for( x <- conf_array ){
+      println(x)
+    }
+
+    val df0 = spark.read.format("csv")
+      .option("sep", ";")
+      .option("header", "false")
+      .load(PointLocation)
+    val df1 = spark.read.format("csv")
+      .option("sep", ";")
+      .option("header", "false")
+      .load(PolygonLocation)
+
+    val geo1 = df0.select("_c0").collect.map(row => row.getString(0))
+    val geo2 = df1.select("_c0").collect.map(row => row.getString(0))
+    println("preference test 1++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    val t1 = System.currentTimeMillis
+    val index = new IndexBuilder("RTREE")
+    index.insert(geo2)
+    val t2 = System.currentTimeMillis
+    println((t2 - t1)/1000.0 + " secs")
+    println("preference test 2++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    val broadcastVar = spark.sparkContext.broadcast(index)
+    println(broadcastVar.value)
+    val joincase = new IndexedJoin(broadcastVar)
+    val t3 = System.currentTimeMillis
+    println((t3 - t2)/1000.0 + " secs")
+    println("preference test 3++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    val result = joincase.join(geo1)
+    val t4 = System.currentTimeMillis
+    println((t4 - t3)/1000.0 + " secs")
+    println("preference test 4++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+//    result.foreach{ out =>
+//      println(out)
+//    }
   }
+
 }
